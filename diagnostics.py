b35375cd640450f643e67b66b617d3de181ddc1a
atabas <anika.tabassum@ryerson.ca>
2021-12-11 22:44:46 -0500

    Diagnostics

--- diagnostics.py
+++ diagnostics.py
@@ -1,40 +1,73 @@
 
 import pandas as pd
 import numpy as np
-import timeit
+import time
 import os
 import json
+import pickle
+import subprocess as sp
+import sys
 
 ##################Load config.json and get environment variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+test_data_path = os.path.join(config['test_data_path'])
+prod_deployment_path = os.path.join(config["prod_deployment_path"])
 
 ##################Function to get model predictions
-def model_predictions():
+def model_predictions(dataset):
     #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+    with open(os.path.join(prod_deployment_path, 'trainedmodel.pkl'), 'rb') as f:
+        model = pickle.load(f)
+    test_dataset_path = os.path.join(test_data_path, dataset)
+    df = pd.read_csv(test_dataset_path)
+    X_test = df.loc[
+        :, ["lastmonth_activity", "lastyear_activity", "number_of_employees"]
+    ]
+    predictions = model.predict(X_test)
+    return predictions #return value should be a list containing all predictions
 
 ##################Function to get summary statistics
 def dataframe_summary():
     #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+    df = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+    X = df.loc[: ,['lastmonth_activity','lastyear_activity','number_of_employees']]
+
+    summary_stats = ((col, [X[col].mean(), X[col].median(), X[col].std()]) for col in X)
+    return (list(summary_stats)) #return value should be a list containing all summary statistics
+
+def dataframe_missing_values():
+    # Calculate percentage of missing values
+    df = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+    return df.isna().sum() / df.count().sum()
 
 ##################Function to get timings
 def execution_time():
     #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    start_time_training = time.time()
+    process = sp.call(['python', 'training.py'])
+    end_time_training = time.time() - start_time_training
+
+    start_time_ingestion = time.time()
+    process = sp.call(['python', 'ingestion.py'])
+    end_time_ingestion = time.time() - start_time_ingestion
+    return [end_time_training, end_time_ingestion] #return a list of 2 timing values in seconds
 
 ##################Function to check dependencies
 def outdated_packages_list():
-    #get a list of 
+    #get a list of outdated packages
+    args = [sys.executable, "-m", "pip", "list", "--outdated"]
+    results = sp.run(args, capture_output=True, check=True).stdout
+    indented_results = ("\n" + results.decode()).replace("\n", "\n    ")
+    return indented_results
 
 
 if __name__ == '__main__':
-    model_predictions()
+    model_predictions('testdata.csv')
     dataframe_summary()
+    dataframe_missing_values()
     execution_time()
     outdated_packages_list()
 
