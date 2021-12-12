e15bf29eaaaa7b08b8bb7f12ee3b3a102a7e3272
atabas <anika.tabassum@ryerson.ca>
2021-12-12 17:02:34 -0500

    Reporting

--- diagnostics.py
+++ diagnostics.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 import time
@@ -9,17 +8,17 @@ import subprocess as sp
 import sys
 
 ##################Load config.json and get environment variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+with open("config.json", "r") as f:
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path'])
+dataset_csv_path = os.path.join(config["output_folder_path"])
+test_data_path = os.path.join(config["test_data_path"])
 prod_deployment_path = os.path.join(config["prod_deployment_path"])
 
 ##################Function to get model predictions
 def model_predictions(dataset):
-    #read the deployed model and a test dataset, calculate predictions
-    with open(os.path.join(prod_deployment_path, 'trainedmodel.pkl'), 'rb') as f:
+    # read the deployed model and a test dataset, calculate predictions
+    with open(os.path.join(prod_deployment_path, "trainedmodel.pkl"), "rb") as f:
         model = pickle.load(f)
     test_dataset_path = os.path.join(test_data_path, dataset)
     df = pd.read_csv(test_dataset_path)
@@ -27,52 +26,55 @@ def model_predictions(dataset):
         :, ["lastmonth_activity", "lastyear_activity", "number_of_employees"]
     ]
     predictions = model.predict(X_test)
-    return predictions #return value should be a list containing all predictions
+    return predictions  # return value should be a list containing all predictions
+
 
 ##################Function to get summary statistics
 def dataframe_summary():
-    #calculate summary statistics here
-    df = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
-    X = df.loc[: ,['lastmonth_activity','lastyear_activity','number_of_employees']]
+    # calculate summary statistics here
+    df = pd.read_csv(os.path.join(dataset_csv_path, "finaldata.csv"))
+    X = df.loc[:, ["lastmonth_activity", "lastyear_activity", "number_of_employees"]]
 
     summary_stats = ((col, [X[col].mean(), X[col].median(), X[col].std()]) for col in X)
-    return (list(summary_stats)) #return value should be a list containing all summary statistics
+    return list(
+        summary_stats
+    )  # return value should be a list containing all summary statistics
+
 
 def dataframe_missing_values():
     # Calculate percentage of missing values
-    df = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+    df = pd.read_csv(os.path.join(dataset_csv_path, "finaldata.csv"))
     return df.isna().sum() / df.count().sum()
+
 
 ##################Function to get timings
 def execution_time():
-    #calculate timing of training.py and ingestion.py
+    # calculate timing of training.py and ingestion.py
     start_time_training = time.time()
-    process = sp.call(['python', 'training.py'])
+    process = sp.call(["python", "training.py"])
     end_time_training = time.time() - start_time_training
 
     start_time_ingestion = time.time()
-    process = sp.call(['python', 'ingestion.py'])
+    process = sp.call(["python", "ingestion.py"])
     end_time_ingestion = time.time() - start_time_ingestion
-    return [end_time_training, end_time_ingestion] #return a list of 2 timing values in seconds
+    return [
+        end_time_training,
+        end_time_ingestion,
+    ]  # return a list of 2 timing values in seconds
+
 
 ##################Function to check dependencies
 def outdated_packages_list():
-    #get a list of outdated packages
+    # get a list of outdated packages
     args = [sys.executable, "-m", "pip", "list", "--outdated"]
     results = sp.run(args, capture_output=True, check=True).stdout
     indented_results = ("\n" + results.decode()).replace("\n", "\n    ")
     return indented_results
 
 
-if __name__ == '__main__':
-    model_predictions('testdata.csv')
+if __name__ == "__main__":
+    model_predictions("testdata.csv")
     dataframe_summary()
     dataframe_missing_values()
     execution_time()
     outdated_packages_list()
-
-
-
-
-
-    
--- reporting.py
+++ reporting.py
@@ -7,26 +7,47 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 import os
-
+from diagnostics import model_predictions
+import seaborn as sns
 
 
 ###############Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+with open("config.json", "r") as f:
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-
-
+dataset_csv_path = os.path.join(config["output_folder_path"])
+test_data_path = os.path.join(config["test_data_path"])
+output_model_path = os.path.join(config["output_model_path"])
 
 
 ##############Function for reporting
-def score_model():
-    #calculate a confusion matrix using the test data and the deployed model
-    #write the confusion matrix to the workspace
+def score_model(test_data_path, test_data_csv_name):
+    # calculate a confusion matrix using the test data and the deployed model
+    # write the confusion matrix to the workspace
+    print(type(test_data_path), type(test_data_csv_name))
+    test_dataset_path = os.path.join(test_data_path, test_data_csv_name)
+    df = pd.read_csv(test_dataset_path)
+
+    predictions = model_predictions(test_data_csv_name)
+    y_test = df["exited"]
+    cfm = metrics.confusion_matrix(y_test, predictions)
+
+    # Credit for confusion matrix code:
+    # https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea
+    group_names = ["True Neg", "False Pos", "False Neg", "True Pos"]
+    group_counts = ["{0:0.0f}".format(value) for value in cfm.flatten()]
+    group_percentages = [
+        "{0:.2%}".format(value) for value in cfm.flatten() / np.sum(cfm)
+    ]
+    labels = [
+        f"{v1}\n{v2}\n{v3}"
+        for v1, v2, v3 in zip(group_names, group_counts, group_percentages)
+    ]
+    labels = np.asarray(labels).reshape(2, 2)
+    sns.heatmap(cfm, annot=labels, fmt="", cmap="Blues")
+
+    plt.savefig(os.path.join(output_model_path, "confusionmatrix.png"))
 
 
-
-
-
-if __name__ == '__main__':
-    score_model()
+if __name__ == "__main__":
+    score_model(test_data_path, "testdata.csv")
